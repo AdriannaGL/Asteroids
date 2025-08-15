c89e34762af023959e44c6be9931f29b20726ef2
AdriannaGL <ada.m.gorska@gmail.com>
2025-08-15 10:04:24 +0200

    circleshape file, refactor of previous steps

--- main.py
+++ main.py
@@ -7,16 +7,19 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: { SCREEN_HEIGHT}")	
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    Clock = pygame.time.Clock()
+    clock = pygame.time.Clock()
     dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(color="black", rect=None, special_flags=0)
+            
+        screen.fill("black")
         pygame.display.flip()
-        
-        dt = (Clock.tick(60))/1000
+
+        # limit the framerate to 60 FPS
+        dt = (clock.tick(60))/1000
+
 if __name__ == "__main__":
     main()
